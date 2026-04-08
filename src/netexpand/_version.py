52c317d0503cfce3a690edd6038c8e4d7493b96d
sjtrotter <stephen.j.trotter@gmail.com>
2026-04-07 19:29:13 -0500

    retry

--- src/netexpand/_version.py
+++ src/netexpand/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.5.1.dev0+g9c47978f0.d20260408'
-__version_tuple__ = version_tuple = (0, 5, 1, 'dev0', 'g9c47978f0.d20260408')
+__version__ = version = '0.5.0'
+__version_tuple__ = version_tuple = (0, 5, 0)
 
 __commit_id__ = commit_id = 'g9c47978f0'
