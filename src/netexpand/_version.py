9c47978f0490f3dd3c9846baa77748c6371dbfdd
sjtrotter <stephen.j.trotter@gmail.com>
2026-04-07 19:23:46 -0500

    finalizing package structure for pypi

--- src/netexpand/_version.py
+++ src/netexpand/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.5.1.dev1+gf2459909e.d20260408'
-__version_tuple__ = version_tuple = (0, 5, 1, 'dev1', 'gf2459909e.d20260408')
+__version__ = version = '0.5.1.dev2+ga3a9e0676.d20260408'
+__version_tuple__ = version_tuple = (0, 5, 1, 'dev2', 'ga3a9e0676.d20260408')
 
-__commit_id__ = commit_id = 'gf2459909e'
+__commit_id__ = commit_id = 'ga3a9e0676'
